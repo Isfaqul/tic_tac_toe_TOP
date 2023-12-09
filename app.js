8d5516a5a3d102644825d3b9d4ab34a1311103c4
Isfaqul <isfaq.xperia@gmail.com>
2023-12-09 22:02:13 +0530

    Modify Game Logic to Support Display Controller WIP

--- app.js
+++ app.js
@@ -1,132 +1,54 @@
-const gameboard = function () {
-  let board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+const gameboard = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
+  // api to access board
   const getBoard = () => board;
 
+  // api to add marker to board
   const addMarker = (index, player) => {
-    board[index - 1] = player;
+    board[index] = player;
   };
 
-  const printBoard = () => {
-    let printedBoard = "";
-    boardCopy = [...getBoard()];
-
-    while (boardCopy[0]) {
-      printedBoard += `${boardCopy.splice(0, 3).join(" | ")}\n`;
-    }
-
-    console.log(printedBoard);
-  };
-
+  // api to reset board
   const resetBoard = () => {
-    board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+    board = ["", "", "", "", "", "", "", "", ""];
   };
 
-  return { getBoard, addMarker, printBoard, resetBoard };
-};
+  return { getBoard, addMarker, resetBoard };
+})();
 
-const gameController = function (player1 = "Player 1", player2 = "Player 2") {
-  let board = gameboard();
+const gameController = (function (
+  playerOne = "Player 1",
+  playerTwo = "Player 2"
+) {
+  // board access through gameboard API
+  let board = gameboard.getBoard();
 
+  // player list
   const players = [
-    {
-      name: player1,
-      marker: "X",
-    },
-    {
-      name: player2,
-      marker: "O",
-    },
+    { name: playerOne, mark: "X" },
+    { name: playerTwo, mark: "O" },
   ];
 
+  // setting active player as p1
   let activePlayer = players[0];
 
+  // method to switch player
   const switchPlayer = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    activePlayer === players[0] ? players[1] : players[0];
   };
 
-  const printNewRound = () => {
-    console.log(`It's ${activePlayer.name}'s turn -`);
-    board.printBoard();
-  };
+  // api to get activePlayer
+  const getActivePlayer = () => activePlayer;
 
+  // method to PlayRound
   const playRound = (index) => {
-    if (
-      board.getBoard()[index - 1] === players[0].marker ||
-      board.getBoard()[index - 1] === players[1].marker
-    ) {
-      console.log(`\t>> Invalid move! : cell not empty <<`);
-      printNewRound();
-    } else {
-      console.log(`\tAdded ${activePlayer.name}'s marker.`);
-      board.addMarker(index, activePlayer.marker);
-
-      let winner = checkWinner();
-      if (winner && winner !== "tie") {
-        board.printBoard();
-        console.log(`ANDD!! ${winner} WINS THE GAME!`);
-        gameReset();
-      } else if (winner === "tie") {
-        board.printBoard();
-        gameReset();
-        console.log(`THE GAME WAS A TIE`);
-      } else {
-        switchPlayer();
-        printNewRound();
-      }
+    if (board[index] === "X" || board[index] === "O") {
+      return "Invalid move, cell already filled.";
+    } else if (board[index] !== "X" || board[index] !== "O") {
+      gameboard.addMarker(index, activePlayer.mark);
     }
   };
 
-  printNewRound();
-
-  let player1MarkerString = players[0].marker.repeat(3); // creating strings such as "XXX"
-  let player2MarkerString = players[1].marker.repeat(3); // to compare with the winning patterns
-
-  const gameReset = () => {
-    activePlayer = players[0];
-    board.resetBoard();
-  };
-
-  const checkWinner = () => {
-    let winner = "";
-    const winPatterns = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    let markerAsPerWinPatterns = [];
-
-    for (let i = 0; i < winPatterns.length; i++) {
-      let string = winPatterns[i]
-        .map((index) => board.getBoard()[index])
-        .join("");
-
-      markerAsPerWinPatterns.push(string);
-    }
-
-    markerAsPerWinPatterns.forEach((pattern) => {
-      if (pattern === player1MarkerString) {
-        winner = players[0].name;
-      } else if (pattern === player2MarkerString) {
-        winner = players[1].name;
-      } else {
-        let copy = markerAsPerWinPatterns.join("");
-        if (!copy.includes("-")) {
-          winner = "tie";
-        }
-      }
-    });
-
-    return winner;
-  };
-
-  return { playRound };
-};
-
-const game = gameController();
+  return { playRound, activePlayer };
+})();
